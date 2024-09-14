c3e5acfef3be16fd2d5f874bc3470d4fa3b73ed9
Bhavyaraj123 <biholabhavyraj@gmail.com>
2024-09-14 17:55:59 +0530

    added website link

--- src/components/Project.js
+++ src/components/Project.js
@@ -36,10 +36,11 @@ const Project = () => {
               <span className="badge bg-blue-500 text-white px-3 py-2 rounded-full">Tailwind</span>
               <span className="badge bg-blue-500 text-white px-3 py-2 rounded-full">Bootstrap</span>
             </div>
-
+          <a href="text-utils-bice-pi.vercel.app" target="_blank" rel="noreferrer noopener">
             <button className="mt-4 py-2 px-4 text-sm font-bold bg-gradient-to-r from-blue-500 to-purple-500 text-white rounded-lg shadow-md hover:opacity-90 hover:scale-105 transition-transform duration-300 ease-in-out flex items-center justify-center">
               View Project <i className="fas fa-arrow-right ml-2"></i>
             </button>
+            </a>
           </div>
 
           <div className="project-card flex flex-col justify-center items-center border border-secondary p-4 bg-gray-50 rounded-lg shadow-md" data-aos="fade-left" data-aos-delay="300">
